80d62dc0ee9daec1c7012aa2c1a0f16e7cd0172f
Kevin Geary <kgeary@gmail.com>
2020-03-17 15:20:13 -0500

    Mobile Nav Drawer working

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import { AppBar, Typography, Toolbar, Container } from "@material-ui/core";
+import { AppBar, Typography, Toolbar, Container, Drawer, Button, List } from "@material-ui/core";
 import "./style.css";
 
+
+
+
 function Nav(props) {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleDrawer = (open) => event => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+    setIsOpen(open);
+  };
+
   return (
     <AppBar position="static" color="default" className="appBar">
       <Toolbar className="toolbar">
@@ -11,10 +22,26 @@ function Nav(props) {
           <Typography variant="h4" noWrap className="navBrand">
             Kevin Geary
           </Typography>
-          <nav>
+          <nav className="large-screen">
             <NavLink to="/" className="navLink" exact activeClassName="active">About</NavLink>
             <NavLink to="/contact" className="navLink" activeClassName="active">Contact</NavLink>
             <NavLink to="/portfolio" className="navLink" activeClassName="active">Portfolio</NavLink>
+          </nav>
+          <nav className="small-screen">
+            <Button
+              style={{ color: "#99BBEE", borderColor: "#99BBEE" }}
+              variant="outlined"
+              onClick={toggleDrawer(true)}
+            >
+              MENU
+            </Button>
+            <Drawer open={isOpen} onClose={toggleDrawer(false)}>
+              <List onClick={toggleDrawer(false)} style={{ display: "flex", flexDirection: "column" }}>
+                <NavLink to="/" className="mobileLink" exact activeClassName="active">About</NavLink>
+                <NavLink to="/contact" className="mobileLink" activeClassName="active">Contact</NavLink>
+                <NavLink to="/portfolio" className="mobileLink" activeClassName="active">Portfolio</NavLink>
+              </List>
+            </Drawer>
           </nav>
         </Container>
       </Toolbar>
