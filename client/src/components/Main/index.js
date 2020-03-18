5dfe9d937cff7404aca7285c325bdc051d0b373c
Kevin Geary <kgeary@gmail.com>
2020-03-18 14:20:37 -0500

    Scroll to top of page when a new page is rendered

--- client/src/components/Main/index.js
+++ client/src/components/Main/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Paper } from "@material-ui/core";
 import "./style.css";
 
 function Main(props) {
 
   const className = "Main";
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   return (
     <Paper className={className} >
